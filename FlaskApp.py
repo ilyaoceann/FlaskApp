7fc463c66a841bcc175ca3336c481364a34586da
Ilya Gomel <lookatjeez@gmail.com>
2018-01-01 20:11:23 +0200

    Added jQuery
    TODO:
    1 - proper registration to mySQL
    2 - Signin in

--- FlaskApp.py
+++ FlaskApp.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
+from flask.ext.mysql import MySQL
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+mysql = MySQL()
+
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'bangarang12'
+app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 
 @app.route('/')
@@ -8,5 +18,45 @@ def hello_world():
     return render_template('index.html')
 
 
+@app.route('/showSignUp')
+def show_sign_up():
+    return render_template('signup.html')
+
+
+@app.route('/signUp', methods=['POST'])
+def sign_up():
+
+    # enter name, email  and password
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+
+    if _name and _email and _password:
+
+        conn = mysql.connect()
+
+        cursor = conn.cursor()
+
+        _hashed_password = generate_password_hash(_password)
+
+        cursor.callproc('sp_createUser', (_name, _email, _hashed_password))
+
+        data = cursor.fetchall()
+
+        if len(data) is 0:
+            conn.commit()
+
+            return json.dumps({'message': 'User created successfully !'})
+        else:
+
+            return json.dumps({'error': str(data[0])})
+
+    else:
+        return json.dumps({'html': '<span>Enter the required fields</span>'})
+
+    cursor.close()
+    conn.close()
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5002)
